d579ed98db35d4aeae54493e2a98fc5359293f40
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-02 15:31:19 +0700

    Fetch data categories from API

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,3 +1,33 @@
-export default function Page() {
-	return <p>Dashboard Page</p>;
+const fetchCategories = async () => {
+	const res = await fetch("https://v1.appbackend.io/v1/rows/QSnvVuqR8Kdh");
+	const { data } = await res.json();
+	return data;
+};
+
+export default async function Home() {
+	const categories = await fetchCategories();
+	console.log(categories);
+	return (
+		<div className="children-wrapper md:flex gap:5">
+			<section className="section-categories p-3 bg-primary md:w-1/2 w-full border">
+				<div>{/* <TitlePage /> */}</div>
+				<div className="search-and-button-category flex justify-between">
+					{/* <SearchInput />
+					<CategoryButton /> */}
+				</div>
+				<div className="wrapper-category-list  p-5">
+					{/* mapping the fetched category list */}
+					{categories.map((category) => {
+						return (
+							<div>
+								<h2>{category.name}</h2>
+								<p>{category.description}</p>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+			<section className="section-quotes p-5 bg-pink-200 flex-1 "></section>
+		</div>
+	);
 }
