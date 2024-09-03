c08f689155c928b33fec52143cf7f7b02e87c040
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-03 13:51:19 +0700

    Create image component for sparkle emoji

---
+++ src/app/ui/sparkle.jsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import SparkleIcon from "../../assets/images/sparkle.png";
+
+export const Sparkle = () => {
+	return <Image src={SparkleIcon} alt="sparkle-emoji-image" width={100} height={100} />;
+};
