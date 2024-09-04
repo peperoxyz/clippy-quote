37128dac9530bcda1d9978b623f0b638451bf83f
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-04 15:07:35 +0700

    Add like quote feature and category badge

--- src/app/ui/components/quote-card.jsx
+++ src/app/ui/components/quote-card.jsx
@@ -7,12 +7,12 @@ import { crimson_text } from "../fonts";
 import YoutubeIcon from "../../../assets/icons/youtube-icon.svg";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { TrashIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 
 export const QuoteCard = ({ quote }) => {
 	const router = useRouter();
 	const [qotd, setQotd] = useState(null);
+	const [vote, setVote] = useState(quote.likes);
 
 	async function handleDeleteData() {
 		await fetch("https://v1.appbackend.io/v1/rows/dfysIrXThAj0", {
@@ -24,6 +24,28 @@ export const QuoteCard = ({ quote }) => {
 		});
 
 		router.refresh();
+	}
+
+	async function sendVoteToDb(quote, vote) {
+		console.log(quote._id, vote);
+		await fetch("https://v1.appbackend.io/v1/rows/dfysIrXThAj0", {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ _id: quote._id, author: quote.author, title: quote.title, transcript: quote.transcript, category: quote.category, likes: vote, downvote: 0 }),
+		});
+
+		router.refresh();
+	}
+
+	function handleVoteData(quote, event) {
+		const voteType = event.target.name;
+		console.log(`Vote type: ${voteType}`, quote);
+		if (voteType === "upvote") {
+			setVote(vote + 1);
+			sendVoteToDb(quote, vote);
+		}
 	}
 
 	function handleQotd(quote) {
@@ -44,18 +66,37 @@ export const QuoteCard = ({ quote }) => {
 		<div key={quote._id} className="text-grey-dark">
 			<div className="flex justify-between w-full">
 				<div className=" w-full hover:bg-primary p-6 border border-l-0 border-r-0 border-b-0 relative">
+					<div className="block md:hidden w-fit mb-2">
+						<div className={` ${backgroundColorClass} text-grey-medium text-xs rounded-6 px-2 py-1`}>{quote.category}</div>
+					</div>
 					<div className={`h-16 top-5 w-[3px] left-0 absolute ${backgroundColorClass}`}></div>
-
-					<h2 className={`${crimson_text.className} text-xl font-bold capitalize`}>{quote.title}</h2>
-					<div className="flex gap-1 items-center">
-						<Image src={YoutubeIcon} alt="clip-quotes-logo-image" width={20} height={20} />
-						<p className="md:text-sm capitalize">{quote.author}</p>
+					<div className="flex justify-between">
+						<div>
+							<h2 className={`${crimson_text.className} text-xl font-bold capitalize`}>{quote.title}</h2>
+							<div className="flex gap-1 items-center">
+								<Image src={YoutubeIcon} alt="clip-quotes-logo-image" width={20} height={20} />
+								<p className="md:text-sm capitalize">{quote.author}</p>
+							</div>
+						</div>
+						<div className="hidden md:block">
+							<div className={` ${backgroundColorClass} text-grey-medium text-xs rounded-6 px-2 py-1`}>{quote.category}</div>
+						</div>
 					</div>
 					<p className="leading-[28px] mt-3 text-grey-medium font-light">&quot;{quote.transcript}&quot;</p>
 					<div className="flex gap-2">
 						<div className="py-3 pb-0 text-[#827F7F]">
 							<button onClick={handleDeleteData} className="font-medium border py-2 px-4 border-[#B9B8B4] rounded-[6px] bg-primary-light hover:bg-[#B9B8B4]/50 hover:text-[#3a3b39]">
 								Delete
+							</button>
+						</div>
+						<div className="py-3 pb-0 text-[#827F7F]">
+							<button onClick={(event) => handleVoteData(quote, event)} name="upvote" className="font-medium border py-2 px-4 border-[#B9B8B4] rounded-[6px] bg-primary-light hover:bg-[#B9B8B4]/50 hover:text-[#3a3b39]">
+								{quote.likes} 👍
+							</button>
+						</div>
+						<div className="py-3 pb-0 text-[#827F7F] hidden">
+							<button onClick={(event) => handleVoteData(quote, event)} name="downvote" className="font-medium border py-2 px-4 border-[#B9B8B4] rounded-[6px] bg-primary-light hover:bg-[#B9B8B4]/50 hover:text-[#3a3b39]">
+								👎🏻👎👎🏽
 							</button>
 						</div>
 						<div className="py-3 pb-0 hidden text-[#827F7F]">
