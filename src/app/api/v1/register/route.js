07f088f44decb7f9dc74cdb19889ee381cae2212
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-12 16:51:52 +0700

    Create route for register feature

---
+++ src/app/api/v1/register/route.js
@@ -0,0 +1,18 @@
+import { prisma } from "/src/utils/prisma";
+import bcrypt from "bcrypt";
+
+export async function POST(req) {
+	const { username, email, password } = await req.json();
+
+	const hashedPassword = await bcrypt.hash(password, 12);
+
+	const newUser = await prisma.user.create({
+		data: {
+			username,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	return Response.json({ data: newUser }, { status: 201 });
+}
