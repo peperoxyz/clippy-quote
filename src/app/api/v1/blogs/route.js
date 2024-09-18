c05679e68f9ec7c4979ba34699dac7583ebc6eb2
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-18 07:52:40 +0700

    Remove route for blogs feature

--- src/app/api/v1/blogs/route.js
+++
@@ -1,6 +0,0 @@
-import { prisma } from "/src/utils/prisma";
-
-export async function GET() {
-	const posts = await prisma.post.findMany();
-	return Response.json({ message: "Getting posts success", posts: posts });
-}
