2bc748526f34f02987acbedc13fc9280a84f87fa
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-18 07:53:36 +0700

    Implement password hashing in register feature

--- src/app/api/v1/users/[id]/route.js
+++ src/app/api/v1/users/[id]/route.js
@@ -1,9 +1,8 @@
 import { prisma } from "/src/utils/prisma";
-
-// beda find unique dengan find first: find first -> yang paling pertama sesuai dengan filter kita. find unique: khusus yang tipe nya unique pada schema
+import bcrypt from "bcrypt";
 
 // GET SINGLE USER BY ID
-export async function GET(request, { params }) {
+export async function GET(_, { params }) {
 	const userId = Number(params.id);
 
 	const user = await prisma.user.findFirst({
@@ -13,33 +12,38 @@ export async function GET(request, { params }) {
 	});
 
 	return Response.json({
-		message: `User with id ${userId} has successfully retreived`,
-		// data: { username, email, password, avatarUrl, address },
+		message: `User data has been successfully retreived`,
 		data: user,
+		status: 200,
 	});
 }
 
 // UPDATE USER BY USER ID
 export async function PATCH(request, { params }) {
-	const { username, email, password, avatarUrl, address } = await request.json();
-
+	const { username, email, password } = await request.json();
 	const userId = Number(params.id);
+
+	// hash the password if it's being updated
+	let hashedPassword;
+	if (password) {
+		hashedPassword = await bcrypt.hash(password, 12);
+	}
 
 	const updatedUser = await prisma.user.update({
 		where: {
 			id: userId,
 		},
-		data: { username, email, password, avatarUrl, address },
+		data: { username, email, password: hashedPassword || undefined },
 	});
 
 	return Response.json({
-		message: `User with id ${userId} has successfully updated`,
+		message: `User data has been successfully updated`,
 		data: updatedUser,
 	});
 }
 
 // DELETE USER BY USER ID
-export async function DELETE(request, { params }) {
+export async function DELETE(_, { params }) {
 	const userId = Number(params.id);
 
 	await prisma.user.delete({
@@ -49,6 +53,6 @@ export async function DELETE(request, { params }) {
 	});
 
 	return Response.json({
-		message: `User with id ${userId} has successfully updated`,
+		message: `User has been successfully deleted`,
 	});
 }
