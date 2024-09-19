b66aad0f85ff90927aeb6ef9a9c08ac4a19ab2e3
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-19 23:35:47 +0700

    Add note comments for further improvement

--- src/app/api/v1/register/route.js
+++ src/app/api/v1/register/route.js
@@ -2,6 +2,11 @@ import { prisma } from "/src/utils/prisma";
 import bcrypt from "bcrypt";
 
 export async function POST(req) {
+	/** Further improvements
+	 * 	1. Add validation check for required fields
+	 * 	2. Collition check -> is the email already registered? if yes, then throw error
+	 */
+
 	const { username, email, password } = await req.json();
 
 	const hashedPassword = await bcrypt.hash(password, 12);
