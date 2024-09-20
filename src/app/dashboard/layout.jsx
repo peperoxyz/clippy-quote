5a905bea65ef24a7d01e5b13c4ef9ba1d1805143
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-20 13:29:37 +0700

    Create protected page to test authorization

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,6 +1,27 @@
 import SideNav from "../ui/components/sidenav";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers"; // which only works in server client, that's why we need js-cookies for the client-component to save cookies
+import { prisma } from "/src/utils/prisma";
 
-export default function Layout({ children }) {
+export default async function Layout({ children }) {
+	// get stored cookies in the browser
+	const storedCookie = cookies();
+	const sessionId = storedCookie.get("sessionId")?.value;
+
+	if (!sessionId) {
+		redirect("/login");
+	}
+
+	// check in the databse, if the session from the cookie browser is valid or exist in the database
+	const isSessionValid = await prisma.session.findUnique({
+		where: {
+			id: sessionId,
+		},
+	});
+
+	if (!isSessionValid) {
+		return redirect("/login");
+	}
 	return (
 		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
 			<div className="w-full md:w-32  flex-none 4">
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,18 +1,13 @@
-import { CqLogo } from "../ui/cq-logo";
-import { crimson_text } from "../ui/fonts";
-import { HeroTitle } from "../ui/components/hero-title";
-import { QuoteCard } from "../ui/components/quotes/quote-card";
-import { CategoryCard } from "../ui/components/category-card";
-
 export default async function Home() {
 	return (
 		<div className="children-wrapper md:flex gap:5">
 			<section className="section-categories bg-primary md:w-1/2 w-full ">
 				<div>
-					<HeroTitle />
+					{/* <HeroTitle /> */}
+					Protected Dashboard: Only Logged In User
 				</div>
 			</section>
-			<section className="section-quotes mt-5 md:mt-0 h-screen md:absolute md:top-0 md:right-0 px-10 py-16 md:w-1/2 bg-primary-light  flex-1 "></section>
+			{/* <section className="section-quotes mt-5 md:mt-0 h-screen md:absolute md:top-0 md:right-0 px-10 py-16 md:w-1/2 bg-primary-light  flex-1 "></section> */}
 		</div>
 	);
 }
