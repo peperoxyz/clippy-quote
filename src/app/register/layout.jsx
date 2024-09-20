6b13291d46d65ce40dbf14553671e60bd3843393
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-20 13:30:03 +0700

    Remove unused imports and fix styling

--- src/app/register/layout.jsx
+++ src/app/register/layout.jsx
@@ -3,7 +3,7 @@ import SideNav from "../ui/components/sidenav";
 export default function Layout({ children }) {
 	return (
 		<div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-			<div className="w-full md:w-32  flex-none 4">
+			<div className="w-full md:w-32 flex-none 4">
 				<SideNav />
 			</div>
 			<div className="flex-grow p-6 md:overflow-y-auto md:p-8 lg:p-12 relative">{children}</div>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,12 +1,8 @@
 import { RegisterForm } from "..//ui/components/register-form";
-import { fetchCategories } from "../lib/fetch-data";
 
 export default async function Home() {
-	const categories = await fetchCategories();
-	console.log(categories);
-
 	return (
-		<section className="-ml-32 h-[80vh] flex flex-col justify-center">
+		<section className="h-[80vh] flex flex-col justify-center">
 			<div className="text-center">
 				<h1 className="text-xl md:text-3xl">Register Account</h1>
 			</div>
