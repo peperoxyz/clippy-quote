5bed55b2851c3fa4a2774ce1dbf5cfd15ea707f2
dea ananda gunawan <deaanandagunawan@gmail.com>
2024-09-22 18:12:21 +0700

    Add content type in header

--- src/app/ui/components/register-form.jsx
+++ src/app/ui/components/register-form.jsx
@@ -19,6 +19,7 @@ export const RegisterForm = () => {
 			const res = await fetch("/api/v1/register", {
 				method: "POST",
 				headers: {
+					"Content-Type:": "application/json",
 					"Access-Control-Allow-Origin": "*",
 					"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 					"Access-Control-Allow-Headers": "Content-Type, Authorization",
